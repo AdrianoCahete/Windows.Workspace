3330551b2e7c4c8df455b22b8d573692f25f1438
Adriano Caheté <nano.rj@gmail.com>
2016-03-25 19:56:52 -0300

    Install Update + Configs

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # WindowsPowerShell.Profile
 My Windows PowerShell Profile
 
+This is intended to people who like my profile, work with me and, more important, me when format or install a new Windows.
+
 ## :warning: WARNING
 **This Install isn't tested and isn't complete! Don't run it!**
+
+** This Profile will replace your profile. Backup and incremetal install are planned.**
+
+### Components
+This profile contains (and obvious install) some components:
+- [Chcolatey](https://chocolatey.org/) - Machine Package Manager, like apt-get, but for Windows.
+- [Scoop](http://scoop.sh/) - A command-line installer for Windows
+- [ConCfg](https://github.com/lukesampson/concfg) - Import/Export Windows console settings 
+- [Pshazz](https://github.com/lukesampson/pshazz) - Pshazz extends your powershell profile
+- [PSReadLine](https://github.com/lzybkr/PSReadLine/) -  A bash inspired readline implementation for PowerShell 
+- [posh-git](http://dahlbyk.github.io/posh-git/) - A PowerShell environment for Git
+- [posh-npm](https://github.com/MSOpenTech/posh-npm) - Powershell-NPM integration 
+
+- If you dont have Powershell 5: [PSGet](http://psget.net/) - Search and install PowerShell modules easy. (PowerShell5 do it natively)
+
+### Scripts
+This profile contains part or full scripts from other sources
+- Modified version of [PoshFunctions Powershell Update](https://github.com/kilasuit/PoshFunctions)
+- [Wormeyman SourceCode Font Install](https://gist.github.com/wormeyman/9041798)
+- 
+
+## TODO
+- Check is admin
+- Backup Powershell profile first
+- Incremental Profile
+- Remove Scoop requirement
+- Install NodeJS
+- Install my tools (from npm)
--- _Install.ps1
+++ _Install.ps1
@@ -28,21 +28,171 @@ function Get-FileEncoding($Path) {
     }
 }
 
+function TestExecutionPolicy {
+	$policy = Get-ExecutionPolicy
+	if ($policy -ne 'Unrestricted') {
+		#echo "`nBefore running this script, run this command: Set-ExecutionPolicy Unrestricted `nFor more info: https://technet.microsoft.com/pt-br/library/ee176961.aspx"
+		Set-ExecutionPolicy Unrestricted -Confirm
+	} else {
+		echo "Execution Policy is: $policy"
+	}
+}
+
+# https://github.com/kilasuit/PoshFunctions
+function UpdatePowerShell {
+	[CmdletBinding()]
+	param()
+	$versionNumber = (Get-WmiObject -class Win32_OperatingSystem |  Select-Object -ExpandProperty version)
+	$versionarray = @()
+	$versionNumber.Split('.') | ForEach-Object { $versionArray += [int]$_}
+	$SimpleVersionNumber = "$($versionArray[0]).$($versionArray[1])"
+	
+	$caption = (Get-WmiObject -class Win32_OperatingSystem | Select-Object -ExpandProperty Caption)
+	$architecture = Get-WmiObject -Class Win32_OperatingSystem |  Select-Object -ExpandProperty OSArchitecture
+	
+	Write-Verbose 'We have Identified your OS and are now determining the Correct package to Download'
+	
+	If ($SimpleVersionNumber -ge 7) { 
+		Write-Warning 'WMF 5 is not installable via this method as you are already running Windows10 or Server 2016'
+	} else {
+		switch ($SimpleVersionNumber) {
+			6.3    {$version = "Windows 2012R2/Win8.1"}
+			6.2    {$version = "Windows 2012/Win8"}
+			6.1    {$version = "Windows 2008R2/Win7"}
+		}
+	}
+	
+	if ($version -eq "Windows 2008R2/Win7") {
+		if ($caption.contains('Windows 7')) {
+			switch ($architecture) {
+				'64-bit' {$version = "Windows 7 64Bit"}
+				'32-bit' {$version = "Windows 7 32Bit"}
+			}
+		} else {
+			$version = "Windows 2008R2"
+		}
+	}
+	elseif($version -eq "Windows 2012R2/Win8.1") {
+		if ($caption.contains('Windows 8.1')) {
+			switch ($architecture) {
+				'64-bit' {$version = "Windows 8.1 64Bit"}
+				'32-bit' {$version = "Windows 8.1 32Bit"}
+			}
+		}
+		else {
+			$version = "Windows 2012R2"
+		}
+	}
+	elseif($version -eq "Windows 2012/Win8") {
+		if ($caption.contains('Windows 8')) {
+			Write-Warning 'Windows 8 is not supported for WMF5 - Sorry about that!'
+		} else {
+			$version = "Windows 2012"
+		}
+	}  
+
+	switch ($Version) {
+		"Windows 2012R2"      {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu"}
+		"Windows 2012"        {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12-KB3134759-x64.msu"}
+		#"Windows 2008R2"      {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win7AndW2K8R2-KB3134760-x64.msu"}    
+		"Windows 8.1 64Bit"   {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu"}
+		"Windows 8.1 32Bit"   {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1-KB3134758-x86.msu"}
+		#"Windows 7 64Bit"     {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win7AndW2K8R2-KB3134760-x64.msu"}
+		#"Windows 7 32Bit"     {$link = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win7-KB3134760-x86.msu"}
+	}
+
+	Write-Verbose 'We are now downloading the correct version of WMF5 for your System'
+	Write-Verbose "System has been Identified as $version"
+	$Request = [System.Net.WebRequest]::Create($link)
+	$Request.Timeout = "100000000"
+	$URL = $Request.GetResponse()
+	$Filename = $URL.ResponseUri.OriginalString.Split("/")[-1]
+	$url.close()
+	
+	$WC = New-Object System.Net.WebClient
+	$WC.DownloadFile($link,"$env:TEMP\$Filename")
+	$WC.Dispose()
+	Write-Verbose 'We are Installing WMF5 for you'
+	Set-Location $env:Temp
+	& .\$Filename
+
+	Start-Sleep 80 # IDK why
+	Remove-Item "$env:TEMP\$Filename"
+	if(Test-path $env:TEMP\WMF4Installed.txt) {
+		Remove-Item $env:Temp\installedWMF4.txt
+	}
+	
+	Write-Verbose 'You need to Reboot after install of WMF4, so you can now proceed to install WMF5'
+	$restartPrompt = Read-Host '[?] Are you sure you want to proceed? [Y] or [N]'
+	if ($restartPrompt -eq 'y' -Or $restartPrompt -eq 'yes') {
+			# Yes
+			echo "`n[!] Restarting in 5 seconds..."
+			shutdown /r /t 5
+		} else {
+			# No 
+			echo "`n/!\ You need to restart manually."
+		}
+	}
+}
+
+
+function Install-SourceProFont {
+	choco install sourcecodepro
+	
+	# $package = 'SourceCodePro'
+
+	 
+	# $fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+	# . $fontHelpersPath
+	 
+	# $fontUrl = 'https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip'
+	# $destination = Join-Path $Env:Temp 'SourceCodePro'
+	 
+	# Install-ChocolateyZipPackage -url $fontUrl -unzipLocation $destination
+	 
+	# $shell = New-Object -ComObject Shell.Application
+	# $fontsFolder = $shell.Namespace(0x14)
+	 
+	# $fontFiles = Get-ChildItem $destination -Recurse -Filter *.otf
+	 
+	# # unfortunately the font install process totally ignores shell flags :(
+	# # http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
+	# # so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
+	# $commands = $fontFiles |
+	# % { Join-Path $fontsFolder.Self.Path $_.Name } |
+	# ? { Test-Path $_ } |
+	# % { "Remove-SingleFont '$_' -Force;" }
+	 
+	# # http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
+	# $fontFiles |
+	# % { $commands += "Add-SingleFont '$($_.FullName)';" }
+	 
+	# $toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+	# Start-ChocolateyProcessAsAdmin $toExecute
+	 
+	# Remove-Item $destination -Recurse
+
+	# https://gist.github.com/wormeyman/9041798
+	Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont' #Get the properties of TTF
+	Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont' -Name 000 -Value 'Source Code Pro' #Set it to SCP
+	#Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont' #Check to see if we properly set it so that SCP is an option
+}
+
 if ($PSVersionInstalled -ge $PSVersionMinimum) {
 
 	# Get Script Policy 
 	$policy = Get-ExecutionPolicy
 	if ($policy -ne 'Unrestricted') { 
-		echo "`nBefore running this script, run this command: Set-ExecutionPolicy Unrestricted `nFor more info: https://technet.microsoft.com/pt-br/library/ee176961.aspx"
+		TestExecutionPolicy
 	} else {
-
 		# Check if Profile Exists
 		if (!(test-path $PROFILE)) {
 			# Don't exist, so create profile 
 			echo "`n[ Create and populate Profile ]`n"
 			New-Item -path $profile -type file -force
+			# Copy from path\profile\profile.sample.ps1 to $profile 
 		} else {
-			# Exist, replace profile (TODO: create a backup first)
+			# Exist, replace profile
 			echo "`n[ Profile already exist. Proceeding install... ]`n"
 			echo "/!\ This script will be replace your currently Powershell Profile!"
 			$profileReplace = Read-Host '[?] Are you sure you want to proceed? [Y] or [N]'
@@ -51,15 +201,22 @@ if ($PSVersionInstalled -ge $PSVersionMinimum) {
 			if ($profileReplace -eq 'y' -Or $profileReplace -eq 'yes') {
 				# Yes
 				echo "`n[!] Replacing Profile..."
-				# Copy from path\profile\myprofile.ps1 to $profile 
+				# Copy from path\profile\profile.sample.ps1 to $profile 
 			} else {
 				# No, i don't know wtf i'm doing, i need to stop this right now!!!!
-				echo "`n/!\ Profile creation has been canceled by the user.`nYou'll need to make all changes manually."
+				echo "`n/!\ Profile creation has been canceled by user.`nYou'll need to make all changes manually."
+				# open Profile and ProfileSample
 			}
 		}
 		
-		#Install Components [TODO: Create an if sequence]
+		#Install Components
 		echo "-----------------`n`n[!] Installing components..."
+		
+		# Install Chcolatey
+		iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+		
+		# Install SourcePro Font 
+		Install-SourceProFont
 		
 		# Scoop -- http://scoop.sh/
 		echo "+ Installing Scoop..."
@@ -77,6 +234,7 @@ if ($PSVersionInstalled -ge $PSVersionMinimum) {
 		echo "`n+ Installing Pshazz..."
 		scoop install pshazz
 		echo "-- Getting Pshazz Profile..."
+		# Get from Repo
 		pshazz get https://raw.github.com/lukesampson/pshazz/master/themes/msys.json
 		pshazz use msys
 		
@@ -84,7 +242,7 @@ if ($PSVersionInstalled -ge $PSVersionMinimum) {
 			# PSGet -- http://psget.net/
 			echo "`n+ Installing PSGet..."
 			(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
-			echo "[!] You can find new modules for your Powershell environment here: http://psget.net/directory/"
+			echo "[!] You can find new modules for your Powershell environment here: http://psget.net/directory/ `nConsider update your Powershell Install"
 		}
 		
 		# PSReadLine -- https://github.com/lzybkr/PSReadLine/
@@ -102,12 +260,10 @@ if ($PSVersionInstalled -ge $PSVersionMinimum) {
 		
 		# Copy functions to Documents Folder
 		
-		
-
 		# Reload Profile
-		#.$profile
+		.$profile
 	}
 } else {
-	# TODO: Update Powershell automatically -- https://raw.githubusercontent.com/kilasuit/PoshFunctions/master/Scripts/Install-WMF5.ps1
-	echo "Update your Powershell to latest version."
+	echo "Updating your Powershell to latest version."
+	UpdatePowerShell	
 }
