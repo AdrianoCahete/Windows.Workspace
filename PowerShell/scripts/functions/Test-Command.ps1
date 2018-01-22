23eeca4e1bf518068b996bb7fadf5450dec1c349
Adriano Caheté <AdrianoCahete@users.noreply.github.com>
2018-01-22 19:19:40 -0200

    Add Test-Command function

---
+++ PowerShell/scripts/functions/Test-Command.ps1
@@ -0,0 +1,15 @@
+﻿Function Test-Command {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'stop'
+
+    try {
+        if(Get-Command $command){
+            RETURN $true
+        }
+    } Catch {
+        RETURN $false
+    } Finally {
+        $ErrorActionPreference = $oldPreference
+    }
+}
